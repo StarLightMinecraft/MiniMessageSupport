ff662e5317e012ed8590981e62517822b3b811c4
HikariLan <shaokeyibb@126.com>
2022-06-21 01:59:56 +0800

    add support for anvil

--- src/main/java/io/hikarilan/minimessagesupport/MiniMessageSupport.java
+++ src/main/java/io/hikarilan/minimessagesupport/MiniMessageSupport.java
@@ -1,5 +1,6 @@
 package io.hikarilan.minimessagesupport;
 
+import com.destroystokyo.paper.event.block.AnvilDamagedEvent;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
@@ -9,6 +10,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -40,9 +45,21 @@ public final class MiniMessageSupport extends JavaPlugin implements Listener {
         });
     }
 
+    @EventHandler
+    private void onClickAnvil(InventoryClickEvent e) {
+        if (e.getInventory().getType() != InventoryType.ANVIL) return;
+        if (e.getSlot() != 2) return;
+        AnvilInventory anvil = (AnvilInventory) e.getInventory();
+        if (anvil.getRenameText() == null) return;
+        if (anvil.getResult() == null) return;
+        anvil.getResult().editMeta(it -> {
+            it.displayName(fromMiniMessage(Component.text(anvil.getRenameText())));
+        });
+    }
+
     private Component fromMiniMessage(Component message) {
         if (!message.children().isEmpty()) {
-           return message.children(message.children().stream().map(this::fromMiniMessage).toList());
+            return message.children(message.children().stream().map(this::fromMiniMessage).toList());
         }
         if (message instanceof TextComponent t) {
             return miniMessage.deserialize(t.content());
